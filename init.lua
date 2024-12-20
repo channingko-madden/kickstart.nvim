3820bc3c81ae00a43bebaa5c3f6e2d1eb6dec458
Channing Ko-Madden <channing@regrow.ag>
2024-12-19 20:56:28 -0500

    Lower pyright strictness to standard

--- init.lua
+++ init.lua
@@ -678,7 +678,8 @@ require('lazy').setup({
             },
             python = {
               analysis = {
-                typeCheckingMode = 'strict',
+                typeCheckingMode = 'standard',
+                reportMissingTypeStubs = false,
               },
             },
           },
