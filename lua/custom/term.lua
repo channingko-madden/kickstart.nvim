75ff6eb354fe69c1ca40547721b58ca295b0a37b
Channing Ko-Madden <channing@regrow.ag>
2025-01-07 14:18:57 -0500

    Set line numbers off in terminal mode

--- lua/custom/term.lua
+++ lua/custom/term.lua
@@ -4,7 +4,17 @@ local function set_terminal_keymaps()
   vim.keymap.set('t', '<Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode', noremap = true })
 end
 
+local function set_terminal_options()
+  vim.opt.number = false
+  vim.opt.relativenumber = false
+end
+
+local function term_open_callback()
+  set_terminal_keymaps()
+  set_terminal_options()
+end
+
 vim.api.nvim_create_autocmd('TermOpen', {
   pattern = [[term://*]],
-  callback = set_terminal_keymaps,
+  callback = term_open_callback,
 })
