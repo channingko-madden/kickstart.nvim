62b91fb26c47bdb2c9e098eddfc10043960dea1c
Channing Ko-Madden <channingkomadden@gmail.com>
2026-05-03 19:19:30 -0400

    Format on buffer save

--- lua/custom/langgroups.lua
+++ lua/custom/langgroups.lua
@@ -68,3 +68,19 @@ vim.api.nvim_create_autocmd('FileType', {
 })
 
 vim.wo.relativenumber = true -- relative line numbers!
+
+-- format on save
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('lsp', { clear = true }),
+  callback = function(args)
+    -- 2
+    vim.api.nvim_create_autocmd('BufWritePre', {
+      -- 3
+      buffer = args.buf,
+      callback = function()
+        -- 4 + 5
+        vim.lsp.buf.format { async = false, id = args.data.client_id }
+      end,
+    })
+  end,
+})
